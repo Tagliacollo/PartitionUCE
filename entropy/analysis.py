16bc14641a913c71c124eb332b51294885a0fe3d
Tagliacollo <victor_tagliacollo@yahoo.com.br>
2016-09-02 14:18:54 +1000

    Testing entropy_calc
    The function to estimate values of entropy seems to be working well!

--- entropy/analysis.py
+++ entropy/analysis.py
@@ -1,2 +1,9 @@
+from functions import *
+import numpy as np
+
 # test the entropy function
 p1 = np.array([0.25, 0.25, 0.25, 0.25]) # should be an entropy 2
+entropy_calc(p1)
+
+p2 = np.array([0.0, 0.0, 0.5, 0.5]) # should be an entropy of 1
+entropy_calc(p2)
