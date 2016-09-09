78a1ed3cec68b29a8f7edbfcb80617fcbdda3d34
roblanf <rob.lanfear@gmail.com>
2016-09-09 17:12:18 +1000

    added a new dataset
    Moyle 2016, more birds (Oscines).

--- scripts/plots.r
+++ scripts/plots.r
@@ -22,10 +22,15 @@ Faircloth = load.dataset("Faircloth_2013")
 Faircloth$dataset = "Faircloth_2013"
 McCormack = load.dataset("McCormack_2013")
 McCormack$dataset = "McCormack_2013"
+Meiklejohn = load.dataset("Meiklejohn_2016")
+Meiklejohn$dataset = "Meiklejohn_2016"
+Crawford = load.dataset("Crawford_2012")
+Crawford$dataset = "Crawford_2012"
+
 
 
 # bind it all up and reorder the factor
-data = rbind(Faircloth, McCormack)
+data = rbind(Faircloth, McCormack, Meiklejohn)
 
 # the picture acros all UCEs together
 p1 = ggplot(data, aes(x = site, y = value))
@@ -33,7 +38,9 @@ p1 + geom_smooth() + facet_grid(type~dataset, scales = "free")
 
 # the picture across all UCEs individually
 p2 = ggplot(data, aes(x = site, y = value, group = name))
-p2 + geom_line(stat="smooth", method = "loess", size = 0.1, alpha = 0.1) + facet_grid(type~dataset, scales = 'free')
+p2 + geom_line(stat="smooth", method = "loess", size = 0.1, alpha = 0.2) + facet_grid(type~dataset, scales = 'free')
+
+
 
 # look at individual UCEs with point data (shows there's not much info on a site-by-site basis)
 for(i in sample(1:length(levels(ent$name)), 10)){
