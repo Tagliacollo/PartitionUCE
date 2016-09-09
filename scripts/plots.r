84d9dedc1e01d48bdaa0f1147d3f66c53583a7c8
roblanf <rob.lanfear@gmail.com>
2016-09-09 15:49:44 +1000

    better plots

--- scripts/plots.r
+++ scripts/plots.r
@@ -2,29 +2,22 @@ library(ggplot2)
 
 ent = read.csv("~/Documents/github/PartitionUCE/processed_data/Crawford_2012_entropy.csv")
 gc  = read.csv("~/Documents/github/PartitionUCE/processed_data/Crawford_2012_gc.csv")
+tig = read.csv("~/Documents/github/PartitionUCE/processed_data/Crawford_2012_tiger.csv")
 
-names(ent) = c("name", "site", "value")
 ent$type = "entropy"
-
-names(gc) = c("name", "site", "value")
 gc$type = "gc"
+tig$type = "tiger"
 
-crawford = rbind(ent, gc)
+crawford = rbind(ent, gc, tig)
 
 # the picture acros all UCEs together
-p1 = ggplot(ent, aes(x = site, y = entropy))
-p1 + geom_smooth()
+p1 = ggplot(crawford, aes(x = site, y = value))
+p1 + geom_smooth() + facet_wrap(~type, scales = "free_y", ncol = 1)
 
-g1 = ggplot(gc, aes(x = site, y = gc))
-g1 + geom_smooth()
 
 # the picture across all UCEs individually
-p2 = ggplot(ent, aes(x = site, y = entropy, group = name))
-p2 + geom_line(stat="smooth", method = "loess", size = 0.1, alpha = 0.3)
-
-g2 = ggplot(gc, aes(x = site, y = gc, group = name))
-g2 + geom_line(stat="smooth", method = "loess", size = 0.1, alpha = 0.3)
-
+p2 = ggplot(crawford, aes(x = site, y = value, group = name))
+p2 + geom_line(stat="smooth", method = "loess", size = 0.1, alpha = 0.3) + facet_wrap(~type, scales = "free_y", ncol = 1)
 
 # look at individual UCEs with point data (shows there's not much info on a site-by-site basis)
 for(i in sample(1:length(levels(ent$name)), 10)){
