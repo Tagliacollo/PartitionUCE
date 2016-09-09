123bb09dd807350046619e2c3c5ddeb4918eb25c
roblanf <rob.lanfear@gmail.com>
2016-09-09 16:34:02 +1000

    better plots

--- scripts/plots.r
+++ scripts/plots.r
@@ -1,23 +1,39 @@
 library(ggplot2)
 
-ent = read.csv("~/Documents/github/PartitionUCE/processed_data/Crawford_2012_entropy.csv")
-gc  = read.csv("~/Documents/github/PartitionUCE/processed_data/Crawford_2012_gc.csv")
-tig = read.csv("~/Documents/github/PartitionUCE/processed_data/Crawford_2012_tiger.csv")
 
-ent$type = "entropy"
-gc$type = "gc"
-tig$type = "tiger"
+load.dataset = function(dataset_name){
 
-crawford = rbind(ent, gc, tig)
+  base_file = "~/Documents/github/PartitionUCE/processed_data/"
+  
+  ent = read.csv(paste(base_file, dataset_name, "_entropy.csv", sep = ""))
+  tig = read.csv(paste(base_file, dataset_name, "_tiger.csv", sep = ""))
+  gc  = read.csv(paste(base_file, dataset_name, "_gc.csv", sep = ""))
+                                
+  ent$type = "entropy"
+  gc$type = "gc"
+  tig$type = "tiger"
+  
+  dataset = rbind(ent, gc, tig)
+
+  return(dataset)
+}
+
+Faircloth = load.dataset("Faircloth_2013")
+Faircloth$dataset = "Faircloth_2013"
+McCormack = load.dataset("McCormack_2013")
+McCormack$dataset = "McCormack_2013"
+
+
+# bind it all up and reorder the factor
+data = rbind(Faircloth, McCormack)
 
 # the picture acros all UCEs together
-p1 = ggplot(crawford, aes(x = site, y = value))
-p1 + geom_smooth() + facet_wrap(~type, scales = "free_y", ncol = 1)
-
+p1 = ggplot(data, aes(x = site, y = value))
+p1 + geom_smooth() + facet_grid(type~dataset, scales = "free")
 
 # the picture across all UCEs individually
-p2 = ggplot(crawford, aes(x = site, y = value, group = name))
-p2 + geom_line(stat="smooth", method = "loess", size = 0.1, alpha = 0.3) + facet_wrap(~type, scales = "free_y", ncol = 1)
+p2 = ggplot(data, aes(x = site, y = value, group = name))
+p2 + geom_line(stat="smooth", method = "loess", size = 0.1, alpha = 0.1) + facet_grid(type~dataset, scales = 'free')
 
 # look at individual UCEs with point data (shows there's not much info on a site-by-site basis)
 for(i in sample(1:length(levels(ent$name)), 10)){
