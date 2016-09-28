e283c97c0fb4d1fd83ae3f4ed2b4eeedd044ab1b
Tagliacollo <victor_tagliacollo@yahoo.com.br>
2016-09-28 11:21:33 +1000

    set up analysis to run
    Some reorganization to make the analysis to run in my computer

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -3,13 +3,13 @@ from Bio import AlignIO, SeqIO
 import numpy as np
 import sys, os
 
-datasets = ['/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/test.nex',
-			'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Crawford_2012.nex',
-			'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Faircloth_2013.nex',
-			'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/McCormack_2013.nex',
-			'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Meiklejohn_2016.nex',
-			'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Moyle_2016.nex',
-			'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Smith_2014.nex']
+datasets = ['/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/test.nex']
+			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Crawford_2012.nex',
+			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Faircloth_2013.nex',
+			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/McCormack_2013.nex',
+			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Meiklejohn_2016.nex',
+			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Moyle_2016.nex',
+			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Smith_2014.nex']
 
 #		   ["/Users/roblanfear/Documents/github/PartitionUCE/raw_data/test.nex",
 #			"/Users/roblanfear/Documents/github/PartitionUCE/raw_data/Smith_2014.nex",
@@ -27,17 +27,15 @@ for dataset_path in datasets:
 
 	# the order of weights has to be 'entropy', 'gc', 'multi' 
 	weights = np.array([[1, 0, 0],
-					   [0, 1, 0],
-	                   [0, 0, 1],
-	                   [1, 1, 0],
-	                   [1, 0, 1],
-	                   [0, 1, 1],
-	                   [1, 1, 1]])
-	
-	for weight in weights:
-	# TODO write stuff to generate an output filename
-		outfilename = output_paths(dataset_path, weight)
+					    [0, 1, 0],
+	                    [0, 0, 1],
+	                    [1, 1, 0],
+	                    [1, 0, 1],
+	                    [0, 1, 1],
+	                    [1, 1, 1]])
 
-		process_dataset(dataset_path, ['entropy', 'gc', 'multi'], weights = weights, outfilename)
+	for weight in weights:
+		os.chdir(output_paths(dataset_path, weight))
+		process_dataset(dataset_path, ['entropy', 'gc', 'multi'], weights = weight, outfilename = 'VAT_test')
 
 
