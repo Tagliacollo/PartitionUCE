4f2557b6609f1a94d808092231c2cc4cd10dcdcd
Tagliacollo <victor_tagliacollo@yahoo.com.br>
2016-10-01 23:01:22 +1000

    deleted weights from the analysis script
    deleted weights

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -3,8 +3,8 @@ from Bio import AlignIO, SeqIO
 import numpy as np
 import sys, os
 
-datasets = ['/Users/roblanfear/Documents/github/PartitionUCE/raw_data/test.nex']
-			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Crawford_2012.nex',
+datasets = ['/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/test.nex',
+			'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Crawford_2012.nex']
 			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Faircloth_2013.nex',
 			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/McCormack_2013.nex',
 			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Meiklejohn_2016.nex',
@@ -25,19 +25,7 @@ for dataset_path in datasets:
 	print ("\n")
 	print (dataset_path)
 
-	# the order of weights has to be 'entropy', 'gc', 'multi' 
-	weights = np.array([[1, 0, 0],
-					    [0, 1, 0],
-	                    [0, 0, 1],
-	                    [1, 1, 0],
-	                    [1, 0, 1],
-	                    [0, 1, 1],
-	                    [1, 1, 1]])
-
-	weights = np.array([[1, 1, 1]])
-
-	for weight in weights:
-		os.chdir(output_paths(dataset_path, weight))
-		process_dataset(dataset_path, ['entropy', 'gc', 'multi'], weights = weight, outfilename = 'VAT_test')
+	os.chdir(output_paths(dataset_path))
+	process_dataset(dataset_path, ['entropy', 'gc', 'multi'], outfilename = 'datatest')
 
 
