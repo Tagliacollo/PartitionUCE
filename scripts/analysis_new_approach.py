741582d6404bb90de0432408bf31cc41aaaf6043
Tagliacollo <victor_tagliacollo@yahoo.com.br>
2016-10-11 15:58:29 +1100

    script to run analysis of new_approach
    script ready to be used!

---
+++ scripts/analysis_new_approach.py
@@ -0,0 +1,30 @@
+from functions_new_approach import *
+
+
+datasets = ['/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/mini_matrix.nex',
+			'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/test.nex']
+			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Crawford_2012.nex']
+			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Faircloth_2013.nex',
+			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/McCormack_2013.nex',
+			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Meiklejohn_2016.nex',
+			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Moyle_2016.nex',
+			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Smith_2014.nex']
+
+#			['/Users/roblanfear/Documents/github/PartitionUCE/raw_data/Crawford_2012_bugtest.nex']
+#		   ["/Users/roblanfear/Documents/github/PartitionUCE/raw_data/test.nex",
+#			"/Users/roblanfear/Documents/github/PartitionUCE/raw_data/Smith_2014.nex",
+#			"/Users/roblanfear/Documents/github/PartitionUCE/raw_data/Moyle_2016.nex",
+#			"/Users/roblanfear/Documents/github/PartitionUCE/raw_data/Crawford_2012.nex",
+#			"/Users/roblanfear/Documents/github/PartitionUCE/raw_data/Faircloth_2013.nex",
+#			"/Users/roblanfear/Documents/github/PartitionUCE/raw_data/McCormack_2013.nex",
+#			"/Users/roblanfear/Documents/github/PartitionUCE/raw_data/Meiklejohn_2016.nex"]
+
+for dataset_path in datasets:
+
+	print ("\n")
+	print (dataset_path)
+
+	os.chdir(output_paths(dataset_path))
+	phylip_name = os.path.basename(dataset_path).rstrip(".nex")
+
+	process_dataset(dataset_path, phylip_name)
