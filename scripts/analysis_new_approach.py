fac3f7e14399f6d142a6d2a63e111120fce08799
Tagliacollo <victor_tagliacollo@yahoo.com.br>
2016-10-15 10:59:10 +1100

    ran all datasets
    create pFinder for all datasets

--- scripts/analysis_new_approach.py
+++ scripts/analysis_new_approach.py
@@ -1,14 +1,14 @@
 from functions_new_approach import *
 
 
-datasets = ['/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/mini_matrix.nex',
-			'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/test.nex']
-			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Crawford_2012.nex']
-			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Faircloth_2013.nex',
-			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/McCormack_2013.nex',
-			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Meiklejohn_2016.nex',
-			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Moyle_2016.nex',
-			#'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Smith_2014.nex']
+datasets = ['/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/test.nex',
+			'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/mini_matrix.nex',
+			'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Crawford_2012.nex',
+			'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Faircloth_2013.nex',
+			'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/McCormack_2013.nex',
+			'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Meiklejohn_2016.nex',
+			'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Moyle_2016.nex',
+			'/Users/Tagliacollo/Desktop/ANU_Australia/PartitionUCE/raw_data/Smith_2014.nex']
 
 #			['/Users/roblanfear/Documents/github/PartitionUCE/raw_data/Crawford_2012_bugtest.nex']
 #		   ["/Users/roblanfear/Documents/github/PartitionUCE/raw_data/test.nex",
@@ -27,4 +27,4 @@ for dataset_path in datasets:
 	os.chdir(output_paths(dataset_path))
 	phylip_name = os.path.basename(dataset_path).rstrip(".nex")
 
-	process_dataset(dataset_path, phylip_name)
+	process_dataset(dataset_path, min_aln_size = 100, outfilename = phylip_name)
