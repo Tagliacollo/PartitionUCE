2d3b92125f42eb9b77e0aeb306e32847f412e06a
Tagliacollo <victor_tagliacollo@yahoo.com.br>
2016-12-19 16:24:24 +1100

    shell command used to run PF
    added this script to be sure I’ve set up the correct PF commands

---
+++ scripts/PF-shell-commands-VAT.sh
@@ -0,0 +1,5 @@
+#unix shell / PF commands
+
+
+Victor-A-Tagliacollo-MacBook-Pro:~ Tagliacollo$ python2 /Applications/partitionfinder-2.1.1/PartitionFinder.py /Users/Tagliacollo/Desktop/Test --raxml --no-ml-tree --rcluster-percent 10 --weights '1, 0, 0, 0' 
+
